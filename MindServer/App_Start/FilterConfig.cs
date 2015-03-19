6ea1714a3b87dce4fc8a18fbf15077388b69d43e
livelyjp <jonny@jonnypillar.co.uk>
2015-03-19 19:15:54 +0000

    Removed old filters

--- MindServer/App_Start/FilterConfig.cs
+++ MindServer/App_Start/FilterConfig.cs
@@ -8,9 +8,9 @@ namespace MindServer
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new RequireHttpsMvcAttribute());
+            //filters.Add(new RequireHttpsMvcAttribute());
             filters.Add(new RequireHttpsApiAttribute());
-            filters.Add(new RequreSecureConnectionFilter());
+            //filters.Add(new RequreSecureConnectionFilter());
         }
     }
 }
