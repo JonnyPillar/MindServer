e246f83b0cfc58884c1e9dfbdf37ca65cee165b2
livelyjp <jonny@jonnypillar.co.uk>
2015-03-19 19:20:04 +0000

    Trying the other attribute

--- MindServer/App_Start/FilterConfig.cs
+++ MindServer/App_Start/FilterConfig.cs
@@ -8,8 +8,8 @@ namespace MindServer
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            //filters.Add(new RequireHttpsMvcAttribute());
-            filters.Add(new RequireHttpsApiAttribute());
+            filters.Add(new RequireHttpsMvcAttribute());
+            //filters.Add(new RequireHttpsApiAttribute());
             //filters.Add(new RequreSecureConnectionFilter());
         }
     }
