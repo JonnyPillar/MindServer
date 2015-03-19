73c9b6407da4aae2eb2d5c83e85e7fedf86bca0b
livelyjp <jonny@jonnypillar.co.uk>
2015-03-19 19:46:45 +0000

    API Https Redirection working. testing MVC attribute now

--- MindServer/App_Start/FilterConfig.cs
+++ MindServer/App_Start/FilterConfig.cs
@@ -8,7 +8,7 @@ namespace MindServer
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            //filters.Add(new RequireHttpsMvcAttribute());
+            filters.Add(new RequireHttpsMvcAttribute());
             //filters.Add(new RequireHttpsApiAttribute());
             //filters.Add(new RequreSecureConnectionFilter());
         }
