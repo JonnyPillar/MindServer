c5ee7022fb1095c922fde5e2855aa00ac6b3eec6
Jonny Pillar <jonny@jonnypillar.co.uk>
2016-05-14 15:03:47 +0100

    Added Basic Information App Page

---
+++ MindServer/Controllers/Web/InformationController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MindServer.Controllers.Web
+{
+    public class InformationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
